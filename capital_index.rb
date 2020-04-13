ee1073afb5a494b5463b4653bed93d13708a78ba
hughgreethead <hugh.greethead@gmail.com>
2020-04-14 09:25:36 +1000

    capital_index challenge

---
+++ capital_index.rb
@@ -0,0 +1,20 @@
+#Create a method that will take a single string as an argument, and will return an ordered array 
+# containing the index of all capital letters.
+# Return an empty array if no capitals are found.
+
+def capital_index(str)
+	output_arr = []
+	str_arr = str.split("")
+	
+	for i in 0..str_arr.length-1
+		if str_arr[i].match(/[A-Z]/)   
+			output_arr.push(i)
+		end
+	end
+	
+	return output_arr
+end
+
+p capital_index("rABbiT")
+p capital_index("lowercase")
+p capital_index("!@#HI!")
